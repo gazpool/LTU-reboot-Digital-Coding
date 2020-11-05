965f90c4d0d0c34d59410900cb1e511f4ac44a64
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-05 20:53:28 +0000

    3.1.3

---
+++ Scripts/Task-3.1.3.js
@@ -0,0 +1,7 @@
+var x = "1";
+var y = "2";
+var z = 3;
+    console.log(x);
+    alert(y);
+    document.write(z);
+
---
+++ Scripts/Task-3.1.4.js
@@ -0,0 +1,8 @@
+var x = 1;
+var y = 2;
+var z = x + y;
+    console.log(x);
+    alert(y);
+    document.write(z);
+
+ 

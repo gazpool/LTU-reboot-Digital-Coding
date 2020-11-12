783446b190ff60c8263afc0aa3f33b7c0b939dd3
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-12 19:27:34 +0000

    Update Task-3.2.6.js

---
+++ Scripts/Task-3.2.6.js
@@ -0,0 +1,15 @@
+var temperature = 20;
+
+if (temperature < 0 ) {
+console.log('Stay inside');
+} else if (temperature < 30) {
+    console.log('Wear a coat and hat');
+} else if (temperature < 50) {
+    console.log('Wear a coat');
+} else if (temperature > 55 && temperature === 55) {
+    console.log('Temperature is 55, no need for a coat');
+} else if (temperature > 55 || temperature === 55) {
+    console.log('It is boiling, whack on some sun cream'); 
+ } else {
+        console.log('It is very hot, just pants is fine');
+    }

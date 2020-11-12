5a7d8999a3a536147e78a048608426f8e9c67f64
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-12 19:09:46 +0000

    Update Task3.1.6.js

--- Scripts/Task3.1.6.js
+++ Scripts/Task3.1.6.js
@@ -1,4 +1,9 @@
-var preTipTotal = 35
-var tipPerc = 19
-var total = toFixed(total)
-var
+var preTipTotal = 35;
+var tipPerc = 0.10;
+
+
+var billTip = preTipTotal * tipPerc;
+
+var receipt = 'Meal: ' + preTipTotal + 'Tip: ' + billTip + 'Total: ' + (preTipTotal + billTip);
+
+console.log(receipt);

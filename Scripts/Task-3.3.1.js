dfcd2aac763f4f9d4000bc7f3fbb4d96d95acf52
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-12 19:48:30 +0000

    Create Task-3.3.1.js

---
+++ Scripts/Task-3.3.1.js
@@ -0,0 +1,11 @@
+function percentageCalculator(number,percentage) {
+    
+    
+    var percentageOf = (number * percentage) / 100;
+    
+    console.log(percentage + '% of ' + number + ' = ' + percentageOf)
+}
+
+
+
+percentageCalculator(805,10);

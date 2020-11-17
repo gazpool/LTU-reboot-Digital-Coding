b7120dcc41fc3150023e7f8aa7510213a38156d4
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-17 20:24:10 +0000

    Delete Task3.1.6.js

--- Scripts/Task3.1.6.js
+++
@@ -1,9 +0,0 @@
-var preTipTotal = 35;
-var tipPerc = 0.10;
-
-
-var billTip = preTipTotal * tipPerc;
-
-var receipt = 'Meal: ' + preTipTotal + 'Tip: ' + billTip + 'Total: ' + (preTipTotal + billTip);
-
-console.log(receipt);

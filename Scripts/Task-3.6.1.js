18384dd0104e01f2669f51e2dc3769671d8d1b89
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-24 20:37:53 +0000

    Update Task-3.6.1.js

--- Scripts/Task-3.6.1.js
+++ Scripts/Task-3.6.1.js
@@ -1,8 +1,9 @@
 var paragraph = document.querySelector('p');
+var header = document.querySelector('h1');
+var img1 = document.querySelector('img');
 
   paragraph.style.color = 'red';
 
-
-  var header = document.querySelector('h1');
-
   header.style.color = 'blue';
+
+setattribute.img1('src',"https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg)");

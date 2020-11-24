526093b0389be12ebc2d3297ba3944922b7dc672
gazpool <33268280+gazpool@users.noreply.github.com>
2020-11-24 18:16:33 +0000

    Create Task-3.4.3.1.js

---
+++ Scripts/Task-3.4.3.1.js
@@ -0,0 +1,11 @@
+var recipe = {
+    title: 'Carbonara',
+    servings: 4,
+    ingredients: ['Spaghetti','Garlic','Eggs','Parmesan','Panchetta'],
+    method: ['Put spaghetti on to boil', 'Smash garlic and add to pan','Fry panchetta in garlic butter...'],
+    LetsCook: function(){
+        console.log("i'm hungry, lets cook "+ this.Title);
+    }
+
+
+};
